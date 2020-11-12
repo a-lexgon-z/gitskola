597bdf0649a28a8a8b1b87651c2551e1aea612d4
a-lexgon-z <70134920+a-lexgon-z@users.noreply.github.com>
2020-11-12 10:38:47 +0100

    dokumenterat

--- Webutveckling/Flexbox/script.js
+++ Webutveckling/Flexbox/script.js
@@ -5,7 +5,7 @@ function showTime() {
     let min = time.getMinutes(); 
     let sec = time.getSeconds(); 
     am_pm = "AM"; 
-
+// Första raderna är bara för att få sek, min och timmarna påbörjar funktionen
     if (hour > 12) { 
         hour -= 12; 
         am_pm = "PM"; 
@@ -14,15 +14,15 @@ function showTime() {
         hr = 12; 
         am_pm = "AM"; 
     } 
-
+// Det här behövs bara för att få AM och PM fungera
     hour = hour < 10 ? "0" + hour : hour; 
     min = min < 10 ? "0" + min : min; 
     sec = sec < 10 ? "0" + sec : sec; 
 
     let currentTime = hour + ":" 
             + min + ":" + sec + am_pm; 
-
+// Får själva klockan att fungera får den att regristrera nuvarande tiden
     document.getElementById("clock") 
-            .innerHTML = currentTime; 
+            .innerHTML = currentTime; // hämtar klockan från html
 } 
-showTime();
+showTime(); // visar tiden
