a6e2f1a8239ac3a4326cde034ec4872f84296460
a-lexgon-z <70134920+a-lexgon-z@users.noreply.github.com>
2020-12-02 12:25:20 +0100

    idk

--- Programmering/strings.py
+++ Programmering/strings.py
@@ -1,4 +1,24 @@
-namn = input("Namn: ")
-x = len(namn)
+# definierat en funktion som returnerar medelvärdet av två tal
+#def medelvärde(tal1,tal2):
+    # medel är en lokal variabel - syns endast i scopet av funktionen
+    #medel = (tal1+tal2)/2
+    #return medel
 
-print(f"Hej {namn} du har {x} bokstäver i ditt namn")
+#vikt_medel = medelvärde(60,62)
+#print(f"Min medelvikt är {vikt_medel}kg")
+
+import numpy as np
+import matplotlib.pyplot as plt
+def f(x):
+    return x**2
+
+# anropa funktionen med paramtern 5
+print(f(5))
+x = np.random(-10,10)
+y = f(x)
+print(x)
+plt.plot(x,y)
+plot.xlabel("x")
+plot.ylabel("y")
+plt.title("f(x) = x**2")
+plt.show()
